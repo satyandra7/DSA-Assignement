aa08091db3438dada874823c9dd84c724cc0f2f7
Satyandra Pal <53830681+satyandra7@users.noreply.github.com>
2023-02-20 23:06:01 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -2,39 +2,81 @@
 //remove adjacent duplicate characters from the string. The output string should not 
 //have any adjacent duplicates.
 
-#include <bits/stdc++.h>
+//Q4. Given a singly linked list, write a function to find root(n)th element, where n is the 
+//number of elements in the list. Assume value of n is not known in advance.
+
+#include<iostream>
+#include<cmath>
 using namespace std;
 
-vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> v;
-        int max_value=0;
-        vector<int> sub_v(nums.begin() , nums.begin() + (k));
-        for(int i=k ; i<nums.size() ; i++){
-            v.push_back(*max_element(sub_v.begin(),sub_v.end()));
-            sub_v.erase(sub_v.begin());
-            sub_v.push_back(nums[i]);
-        }
-        v.push_back(*max_element(sub_v.begin(),sub_v.end()));
-        return v;
+class node{
+    public:
+    int data;
+    node *next;
+    node(){
+        data = 0;
+        next = NULL;
     }
+    node(int data){
+        this -> data = data;
+        next = NULL;
+    }
+};
+
+node *takeinput(){
+    int data;
+    cout<<"Enter the data (Enter -1 to end the List) = ";
+    cin>>data;
+    node *head=NULL;
+    node *tail = NULL;
+    while(data != -1){
+        node *nnode = new node(data);
+        if(head == NULL){
+            head = nnode;
+            tail = nnode;
+        }
+        else{
+            tail -> next = nnode;       
+            tail = nnode;               
+        }
+        cout<<"Enter the data (Enter -1 to end the List) = ";
+        cin >> data;
+    };
+    return head;
+}
+
+void print(node *head){
+    node *temp=head;
+    while(temp!=NULL){                      
+        cout<<temp->data<<" - > ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+int count(node *temp){
+	int c=0;
+	while(temp->next!=NULL){
+		temp=temp->next;
+		c++;
+	}
+	return c;
+}
+int get_rootEle(node *temp){
+	int c=count(temp);
+	c=sqrt(c);
+    while(temp -> next != NULL && c>0){
+        temp = temp -> next;
+        c--;
+    }
+    return temp->data;
+
+}
 
 int main(){
-    int n;
-    cout<<"Enter the size of array = ";
-    cin>>n;
-    cout<<"Enter the Array = ";
-    vector<int> a;
-    int data;
-    for(int i=0 ; i<n; i++){
-        cin>>data;
-        a.push_back(data);
-    }
-    int w;
-    cout<<"Enter the size of Window = ";
-    cin>>w;
-    vector<int> max;
-    max = maxSlidingWindow(a,w);
-    for(int i=0;i<max.size();i++)
-        cout<<max[i]<<" ";
+    node *head= new node;
+    head = takeinput();
+    cout<<"The Linked List is as follows : "<<endl;
+    print(head);
+    cout<<"The root element of the list = "<<get_rootEle(head)<<endl;
     return 0;
 }
