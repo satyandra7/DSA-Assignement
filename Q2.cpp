0c2c74ed00d8676e582100ca4b7f72650f980203
Satyandra Pal <53830681+satyandra7@users.noreply.github.com>
2023-02-20 23:12:22 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -2,3 +2,31 @@
 //remove adjacent duplicate characters from the string. The output string should not 
 //have any adjacent duplicates.
 
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+void remove(char a[]){
+    if(strlen(a)==1)
+        return ;
+    remove(a+1);
+    if(a[0]==a[1]){
+        for(int i=2;i<=strlen(a);i+=2){
+
+            a[i-2]=a[i];
+            a[i-1]=a[i+1];
+        }
+    }
+    return ;
+}
+
+int main(){
+    char a[100];
+    cout<<"Enter the String = ";
+    cin>>a;
+    cout<<"Original String = "<<a;
+    remove(a);
+    cout<<"\n";
+    cout<<"New String = "<<a;
+    return 0;
+}
